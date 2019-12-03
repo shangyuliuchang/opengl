7176746eaf7b8a8de0ae2e156b65478ae50766e6
chang liu <shangyuliuchang@163.com>
2019-12-03 11:20:44 +0800

    change points to lines

--- time/time/main.cpp
+++ time/time/main.cpp
@@ -1,14 +1,69 @@
 #include<GL/freeglut.h>
-int main() {
+#include<math.h>
+float *funx, *funy;
+int n;
+void display() {
+	glBegin(GL_LINES);
+	glColor3f(1,1,1);
+	glVertex2f(funx[0], funy[0]);
+	for (int i = 0; i < n; i++) {
+		glVertex2f(funx[i], funy[i]);
+		glVertex2f(funx[i], funy[i]);
+	}
+	glEnd();
+	glFlush();
+}
+float f(float x) {
+	return (x*sin(1/x));
+}
+void calculate(float* x, float*y, int* number, float begin, float end) {
+	float n = 3.0f;
+	float delta = (end - begin) / n;
+	for (float mid = begin + delta; mid <= end - delta; mid++) {
+		if (fabs((f(begin)*(end - mid) + f(end)*(mid - begin)) / (end - begin) - f(mid)) > 0.00001f && (*number) < 99999) {
+			x[*number] = mid;
+			y[*number] = f(mid);
+			(*number)++;
+			calculate(x, y, number, begin, mid);
+			calculate(x, y, number, mid, end);
+		}
+	}
+}
+void rerange(float* x, float* y, int n) {
+	float ex;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (x[i] > x[j]) {
+				ex = x[i];
+				x[i] = x[j];
+				x[j] = ex;
+				ex = y[i];
+				y[i] = y[j];
+				y[j] = ex;
+			}
+		}
+	}
+}
+int main(int argc, char *argv[]) {
+	int number = 0;
+	float x[100000], y[100000];
+	x[0] = -0.999f;
+	x[1] = 1.0f;
+	y[0] = f(x[0]);
+	y[1] = f(x[1]);
+	number = 2;
+	calculate(x, y, &number, x[0], x[1]);
+	funx = x;
+	funy = y;
+	n = number;
+	rerange(x, y, number);
 	glutInit(&argc, argv);
-	/* setup the size, position, and display mode for new windows */
 	glutInitWindowSize(1000, 1000);
 	glutInitWindowPosition(0, 0);
 	glutInitDisplayMode(GLUT_RGB);
-	/* create and set up a window */
 	glutCreateWindow("hello, teapot!");
+	glClearColor(0, 0, 0, 1);
 	glutDisplayFunc(&display);
-	/* tell GLUT to wait for events */
 	glutMainLoop();
 	return 0;
 }
