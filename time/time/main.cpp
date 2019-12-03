f9bae4ab79c2a4577dbf9b8e1c571fbea44b0b0b
chang liu <shangyuliuchang@163.com>
2019-12-03 10:17:06 +0800

    firse edit

--- time/time/main.cpp
+++ time/time/main.cpp
@@ -1,15 +1,4 @@
-#include <stdio.h>
-#include<Windows.h>
-#include <time.h>
-int main()
-{
-	time_t t;
-	tm * lt;
-	while (true) {
-		time(&t);//获取Unix时间戳。
-		lt = localtime(&t);//转为时间结构。
-		printf("%d/%d/%d %d:%d:%d\n", lt->tm_year + 1900, lt->tm_mon, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);//输出结果
-		Sleep(1000);
-	}
-	return 0;
+#include<GL/freeglut.h>
+int main() {
+	
 }
